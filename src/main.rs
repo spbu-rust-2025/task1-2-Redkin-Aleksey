eb21c3ea3ebf6c4cb02354d5b9dadb2acbb5eaa0
Redkin-Aleksey <redkin0aleksey@gmail.com>
2025-11-09 21:07:42 +0300

    feat: add homework code to src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::io;
+
 fn main() {
 
+    let mut sum: i64 = 0;
+
+    loop {
+        let mut input = String::new();
+        if let Err(e) = io::stdin().read_line(&mut input) {
+            eprintln!("input error: {}", e);
+            continue;
+        }
+
+        let number: i64 = match input.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("NaN");
+                return
+            }
+        };
+
+        if number == -1 {
+            break;
+        }
+        sum += number;
+    }
+
+    println!("{sum}");
 }
